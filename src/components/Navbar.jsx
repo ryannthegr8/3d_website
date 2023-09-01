fb9b6c39e6d709ef1e380278af418fb113df2464
ryannthegr8 <mmbaleryan@gmail.com>
2023-09-01 22:49:36 +0300

    Complete Nav-Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingx} w-full flex items-start py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logo and name section */}
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="Logo" className="w-12 h-12 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Ryan &nbsp;
+            <span className="sm:block hidden">| Software Developer</span>
+          </p>
+        </Link>
 
-export default Navbar
+        {/* Navigation links */}
+        <ul className="list-none hidden sm:flex flex-row gap-10 pr-4">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }hover: text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        {/* Menu and its icon */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          {/* Icon for the menu */}
+          <img
+            src={toggle ? close : menu}
+            alt="Menu"
+            className="cursor-pointer object-contain w-[28px] h-[28px]"
+            onClick={() => setToggle(!toggle)}
+          />
+          {/* Dropdown menu */}
+          <div
+            className={`${
+              toggle ? "flex" : "hidden"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            {/* Dropdown menu nav-links */}
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
