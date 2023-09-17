b4ff93901cf9b81f83af82b8d3b80c153182b180
ryannthegr8 <mmbaleryan@gmail.com>
2023-09-18 00:33:34 +0300

    to 3D computer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+//for animations
+import { motion } from "framer-motion";
+
+//style
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+      >
+        {/* for the purple dot and line going downwards */}
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-export default Hero
+        {/* Hero text */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Ryan Mmbale</span>
+          </h1>
+
+          {/* Tagline */}
+          <p className={`${styles.heroSubText} text-white-100`}>
+            I develop software developer, user{" "}
+            <br className="sm:block hidden" />
+            user interfaces and web applications
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
